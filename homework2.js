270f46ea1c6a1c98dba3b04ef735711977ecdcf7
JAlvarenga1117 <jeherre6@cougarnet.uh.edu>
2026-03-26 14:15:49 -0500

    Implement reviewForm to show user information
    Add a reviewForm function to display user input.

--- homework2.js
+++ homework2.js
@@ -1,1 +1,8 @@
+function reviewForm() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
 
+    document.getElementById("reviewArea").innerHTML =
+        "<h3>PLEASE REVIEW THIS INFORMATION</h3>" +
+        "<p>Name: " + firstName + " " + lastName + "</p>";
+}
