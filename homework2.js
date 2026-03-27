df80928dc740546201c347b296eb3b9298033a5b
JAlvarenga1117 <jeherre6@cougarnet.uh.edu>
2026-03-27 13:01:51 -0500

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -1,24 +1,24 @@
-function reviewForm() {
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-
-    document.getElementById("reviewArea").innerHTML =
-        "<h3>PLEASE REVIEW THIS INFORMATION</h3>" +
-        "<p>Name: " + firstName + " " + lastName + "</p>";
-}
-
 function updateSlider() {
     document.getElementById("sliderValue").innerText =
         document.getElementById("healthScale").value;
-    function checkPasswords() {
+}
+
+function checkPasswords() {
     const pw1 = document.getElementById("password").value;
     const pw2 = document.getElementById("password2").value;
+    const userId = document.getElementById("userId").value.toLowerCase();
+
+    document.getElementById("userId").value = userId;
 
     if (pw1 !== pw2) {
         alert("Passwords do not match.");
         return false;
     }
 
+    if (pw1.toLowerCase().includes(userId)) {
+        alert("Password cannot contain your user ID.");
+        return false;
+    }
+
     return true;
-}
 }
